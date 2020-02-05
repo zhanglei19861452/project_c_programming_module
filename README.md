41b64f110b0ca9edd9b6064054a23bc628e17ecf
LeiZhang <zhanglei19861452@163.com>
2020-02-05 21:16:50 +0800

    v0001 add show_platform.c

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # project_c_programming_module
 C language programming module
+
+## show_platform.c
+
+1.shows C code that uses casting to access and print the byte representations of different program object. 
+
+主要用来显示开发平台的各种数据类型的宽度。并可以看出是大端模式还是小端模式。
---
+++ show_platform.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+void show_bytes(unsigned char *start,unsigned int len)
+{
+    unsigned int i;
+    for(i = 0;i < len;i ++)
+    {
+        printf("%.2x ",start[i]);
+    }
+    printf("\n");
+}
+
+void show_short(short x)
+{
+    show_bytes((unsigned char *)&x,sizeof(x));
+}
+
+void show_int(int x)
+{
+    show_bytes((unsigned char *)&x,sizeof(x));
+}
+
+void show_float(float x)
+{
+    show_bytes((unsigned char *)&x,sizeof(x));
+}
+
+void show_double(double x)
+{
+    show_bytes((unsigned char *)&x,sizeof(x));
+}
+
+void show_pointer(void *x)
+{
+    show_bytes((unsigned char *)&x,sizeof(x));
+}
+
+
+/*
+ 测试平台的各种数据类型，以及大小端
+ 
+ 测试结果：
+ short 2Bytes
+ int 4Bytes
+ float 4Bytes
+ double 8Bytes
+ pointer 8Bytes
+ CPU 模式：小端
+ */
+void test_show_bytes(int x)
+{
+    short sval = x;
+    int inval = x;
+    float fval = x;
+    double dval = x;
+    int *pval = &x;
+    
+    show_short(sval);
+    show_int(inval);
+    show_float(fval);
+    show_double(dval);
+    show_pointer(pval);
+}
+
+
+
+int main(void)
+{
+    test_show_bytes(0x12345678);
+    
+}
